f4968b3bf6cb460470a395c76de8d07d7cf7f464
rtoland <97182468+rtoland@users.noreply.github.com>
2023-11-07 18:06:15 -0700

    Update app.py

--- app.py
+++ app.py
@@ -4,3 +4,9 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/db_test')
+def testing():
+    conn = psycopg2.connect("postgres://rtolandlab10_db_user:I9LVBtBre4ZwtoIPFXQLH5kMpn0ODD6R@dpg-cl5dpmd6fh7c73elllig-a/rtolandlab10_db")
+    conn.close()
+    return "Database Connection Successful"
